3f84b86d94ffcaec04bca0841fec416ab272f7f7
inaja <inaja@debian>
2014-11-14 17:22:17 -0300

    Criacao dos metodos Salvar/Excluir/Pesquisar e Alterar na classe Cliente

--- src/com/mercado/mb/ClienteMB.java
+++ src/com/mercado/mb/ClienteMB.java
@@ -11,7 +11,9 @@ public class ClienteMB {
 	
 	private Cliente cliente;
 	private AbstractDAO<Cliente> clienteDAO;
-	private List<Cliente> clientes = new ArrayList<Cliente>();
+	private List<Cliente> clientes;
+	
+	private String erro;
 	
 	public ClienteMB(){
 		cliente = new Cliente();
@@ -20,18 +22,37 @@ public class ClienteMB {
 	}
 	
 	public String salvar(){
-		
-		return "sucesso"; //string que leva para pagina de cadastro realizado 
-		
+		try{
+			clienteDAO.adicionar(cliente);
+			this.cliente = new Cliente();
+		} catch (Exception ex) {
+			System.out.println("Erro:" + ex);
+			this.erro = ex.getMessage();
+			return "erro";
+		}		
+		return "sucesso"; //string que leva para pagina de cadastro realizado 		
 	}
 	
-	public String excluir(){
-		
+	public String excluir(Cliente cli){
+		try{
+			clientes.remove(cli);
+			this.cliente = new Cliente();			
+		}catch (Exception ex) {
+			System.out.println("Erro:" + ex);
+			this.erro = ex.getMessage();
+			return "erro";
+		}
 		return "sucesso";//string que leva para pagina de exclusao realizado
 	}
 	
 	public String pesquisar(){
-		
+		try{
+			this.clientes = clienteDAO.getLista(cliente);
+		}catch (Exception ex) {
+			System.out.println("Erro:" + ex);
+			this.erro = ex.getMessage();
+			return "erro";
+		}		
 		return "pesquisar"; //string que leva para pagina de pesquisa
 	}
 	
@@ -40,28 +61,21 @@ public class ClienteMB {
 		return "alterar"; //string que leva para pagina de cadastro
 	}
 
-	public Cliente getCliente() {
-		return cliente;
-	}
-
-	public void setCliente(Cliente cliente) {
-		this.cliente = cliente;
-	}
-	
-	public AbstractDAO<Cliente> getClienteDAO() {
-		return clienteDAO;
-	}
-
-	public void setClienteDAO(AbstractDAO<Cliente> clienteDAO) {
-		this.clienteDAO = clienteDAO;
-	}
-
 	public List<Cliente> getClientes() {
+		clientes = clienteDAO.getLista(cliente);
 		return clientes;
 	}
 
 	public void setClientes(List<Cliente> clientes) {
 		this.clientes = clientes;
+	}
+	
+	public String getErro() {
+		return erro;
+	}
+
+	public void setErro(String erro) {
+		this.erro = erro;
 	}
 
 }
